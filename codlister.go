9616ab46b67abbc0e52a943df2c83e54defc57d8
Imad <baddin2016@gmail.com>
2018-04-01 03:33:17 -0700

    too long comments seperated to multiple lines

--- codlister.go
+++ codlister.go
@@ -2,9 +2,9 @@ package codelister
 
 
 func GetFuncs(str string) []string {
-	/*This function uses all the other functions , it takes a string then it finds all the functions indexes (FindFuncIndexes)
-	then it extracts their names(ExtractFunName()) and their return types (ExtractFuncsReturnType())
-	then it adds them into a slice of strings  and return it.
+	/*This function uses all the other functions, it takes a string then it finds all
+	of the functions indexes (FindFuncIndexes) then it extracts their names(ExtractFunName())
+	and their return types (ExtractFuncsReturnType())then it adds them into a slice of strings and return it.
 	TODO: make every Item in the sclice into a map[string]string as map[function_name]return_type. */
 	
 	var funcNames []string
@@ -22,8 +22,8 @@ func GetFuncs(str string) []string {
 }
 
 func ExtractFuncName(str string, startIndex int) string {
-	/*This function takes a string and a starting index of the function (from FindFuncIndexes) and it will extract the function name untill
-	the first "(" symbol then returns it. */
+	/*This function takes a string and a starting index of the function (from FindFuncIndexes) then it
+	will extract the function name untill the first "(" symbol then returns it. */
 	var name string
 	index := startIndex
 	for string(str[index]) != "(" {
@@ -35,7 +35,7 @@ func ExtractFuncName(str string, startIndex int) string {
 
 func FindFuncIndexes(str string) []int { //make it accepts a word whatever it is and search for its index!
 	/*
-	This function gets a string and return a list of every index that refers to the "func" keyword
+	This function gets a string and return a list of every index that refers to the "func" keyword,
 	the index is for the first word after the keyword "func".
 	*/
 		
@@ -51,8 +51,9 @@ func FindFuncIndexes(str string) []int { //make it accepts a word whatever it is
 
 func FindFuncsReturnType(str string, funcIndex int) string { //make it less longer!
 	/*
-	This func takes a string and the starting index of the keyword "func" (which we will get from the other function "FindFuncIndexes")
-	and it will extract the type of the function in backward starting from the symbol "{" until the first ")" then it will reverse it and return it
+	This func takes a string and the starting index of the keyword "func" (which we will get from 
+	the other function "FindFuncIndexes") and it will extract the type of the function 
+	in backward, starting from the symbol "{" until the first ")" then it will reverse it and return it
 	TODO: fix the "no return function" bug.
 	*/
 	var returnType string
