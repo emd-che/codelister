18815b57488631c5eabfdd2de729d6d27e923a0c
* <baddin2016@gmail.com>
2018-04-05 15:01:10 +0100

    #1 added the 'file' flag to open a 'Go' source file

--- main/main.go
+++ main/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-	"github.com/baddin/codelister"
+	"flag"
 	"fmt"
+	"github.com/baddin/codelister"
+	"log"
+	"io/ioutil"
 )
 
+func readFile(fileName string) string {
+	bytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str := string(bytes)
+	return str
+
+}
 
 func main() {
-	str := "hrllo kdjffunckkk func main(){} ksjkd func hello() string {} skkskks func hi() []int  sdsdjjvyfgfdgdfgfdgu fdgdfgfd func test() int {} ri"
-	for _, v := range codelister.GetFuncs(str) {
-		fmt.Println(v)
+
+	fileName := flag.String("file", "", "file:open the 'Go' source file.")
+	flag.Parse()
+	if *fileName != "" {
+		str := readFile(*fileName)
+		for _, v := range codelister.GetFuncs(str) {
+			fmt.Println(v)
+		}
+
+	} else {
+		fmt.Println("usage: codelister -file <filename>")
 	}
+	
 
 }
